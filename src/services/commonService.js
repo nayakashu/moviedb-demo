35385bb5e775a7f3c399051a370f2570412a01d4
Biswaranjan Rout <biswaranjan.rout@wdc.com>
2019-04-09 00:03:36 +0530

    Optimizing build and deploy process

--- src/services/commonService.js
+++ src/services/commonService.js
@@ -1,7 +1,7 @@
 // Check if movie is already added to user's favorite list
 export const isMovieAdded = (movieId, userMovies) => {
   if (userMovies) {
-    return userMovies.some(item => item['data']['id'] == movieId);
+    return userMovies.some(item => item['data']['id'] === movieId);
   }
   return false;
 };
